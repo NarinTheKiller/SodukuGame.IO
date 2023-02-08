009be980b9de7ce8175efd97d97986a1e07c096a
HP <HP@AMIR-PC>
2023-02-08 14:50:02 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox8 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox9 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox10 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox11 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox12 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox13 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox14 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox15 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox16 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox17 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.richTextBox1.Location = new System.Drawing.Point(8, 8);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "4";
+            this.richTextBox1.ZoomFactor = 6F;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(422, 8);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox2.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.ZoomFactor = 6F;
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox3.Location = new System.Drawing.Point(284, 8);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox3.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.ZoomFactor = 6F;
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox4.Location = new System.Drawing.Point(146, 8);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox4.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox4.TabIndex = 3;
+            this.richTextBox4.Text = "";
+            this.richTextBox4.ZoomFactor = 6F;
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox6.Location = new System.Drawing.Point(422, 248);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox6.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox6.TabIndex = 5;
+            this.richTextBox6.Text = "";
+            this.richTextBox6.ZoomFactor = 6F;
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox7.Location = new System.Drawing.Point(284, 248);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox7.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox7.TabIndex = 6;
+            this.richTextBox7.Text = "";
+            this.richTextBox7.ZoomFactor = 6F;
+            // 
+            // richTextBox8
+            // 
+            this.richTextBox8.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.richTextBox8.Location = new System.Drawing.Point(146, 248);
+            this.richTextBox8.Name = "richTextBox8";
+            this.richTextBox8.ReadOnly = true;
+            this.richTextBox8.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox8.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox8.TabIndex = 7;
+            this.richTextBox8.Text = "6";
+            this.richTextBox8.ZoomFactor = 6F;
+            // 
+            // richTextBox9
+            // 
+            this.richTextBox9.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox9.Location = new System.Drawing.Point(8, 248);
+            this.richTextBox9.Name = "richTextBox9";
+            this.richTextBox9.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox9.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox9.TabIndex = 8;
+            this.richTextBox9.Text = "";
+            this.richTextBox9.ZoomFactor = 6F;
+            // 
+            // richTextBox10
+            // 
+            this.richTextBox10.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox10.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox10.Location = new System.Drawing.Point(422, 128);
+            this.richTextBox10.Name = "richTextBox10";
+            this.richTextBox10.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox10.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox10.TabIndex = 9;
+            this.richTextBox10.Text = "";
+            this.richTextBox10.ZoomFactor = 6F;
+            this.richTextBox10.TextChanged += new System.EventHandler(this.richTextBox10_TextChanged);
+            // 
+            // richTextBox11
+            // 
+            this.richTextBox11.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.richTextBox11.Location = new System.Drawing.Point(284, 128);
+            this.richTextBox11.Name = "richTextBox11";
+            this.richTextBox11.ReadOnly = true;
+            this.richTextBox11.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox11.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox11.TabIndex = 10;
+            this.richTextBox11.Text = "2";
+            this.richTextBox11.ZoomFactor = 6F;
+            // 
+            // richTextBox12
+            // 
+            this.richTextBox12.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox12.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox12.Location = new System.Drawing.Point(146, 128);
+            this.richTextBox12.Name = "richTextBox12";
+            this.richTextBox12.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox12.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox12.TabIndex = 11;
+            this.richTextBox12.Text = "";
+            this.richTextBox12.ZoomFactor = 6F;
+            // 
+            // richTextBox13
+            // 
+            this.richTextBox13.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox13.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox13.Location = new System.Drawing.Point(8, 128);
+            this.richTextBox13.Name = "richTextBox13";
+            this.richTextBox13.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox13.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox13.TabIndex = 12;
+            this.richTextBox13.Text = "";
+            this.richTextBox13.ZoomFactor = 6F;
+            // 
+            // richTextBox14
+            // 
+            this.richTextBox14.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.richTextBox14.Location = new System.Drawing.Point(422, 368);
+            this.richTextBox14.Name = "richTextBox14";
+            this.richTextBox14.ReadOnly = true;
+            this.richTextBox14.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox14.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox14.TabIndex = 13;
+            this.richTextBox14.Text = "8";
+            this.richTextBox14.ZoomFactor = 6F;
+            // 
+            // richTextBox15
+            // 
+            this.richTextBox15.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox15.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox15.Location = new System.Drawing.Point(284, 368);
+            this.richTextBox15.Name = "richTextBox15";
+            this.richTextBox15.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox15.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox15.TabIndex = 14;
+            this.richTextBox15.Text = "";
+            this.richTextBox15.ZoomFactor = 6F;
+            // 
+            // richTextBox16
+            // 
+            this.richTextBox16.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox16.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox16.Location = new System.Drawing.Point(146, 368);
+            this.richTextBox16.Name = "richTextBox16";
+            this.richTextBox16.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox16.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox16.TabIndex = 15;
+            this.richTextBox16.Text = "";
+            this.richTextBox16.ZoomFactor = 6F;
+            // 
+            // richTextBox17
+            // 
+            this.richTextBox17.BackColor = System.Drawing.Color.Sienna;
+            this.richTextBox17.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox17.Location = new System.Drawing.Point(8, 368);
+            this.richTextBox17.Name = "richTextBox17";
+            this.richTextBox17.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox17.Size = new System.Drawing.Size(132, 114);
+            this.richTextBox17.TabIndex = 16;
+            this.richTextBox17.Text = "";
+            this.richTextBox17.ZoomFactor = 6F;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(577, 37);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(157, 15);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "عدد دکمه های آبی ثابت هستند";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(604, 439);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(102, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "اتمام بازی";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.Sienna;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(754, 491);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox17);
+            this.Controls.Add(this.richTextBox16);
+            this.Controls.Add(this.richTextBox15);
+            this.Controls.Add(this.richTextBox14);
+            this.Controls.Add(this.richTextBox13);
+            this.Controls.Add(this.richTextBox12);
+            this.Controls.Add(this.richTextBox11);
+            this.Controls.Add(this.richTextBox10);
+            this.Controls.Add(this.richTextBox9);
+            this.Controls.Add(this.richTextBox8);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private RichTextBox richTextBox2;
+        private RichTextBox richTextBox3;
+        private RichTextBox richTextBox4;
+        private RichTextBox richTextBox6;
+        private RichTextBox richTextBox7;
+        private RichTextBox richTextBox8;
+        private RichTextBox richTextBox9;
+        private RichTextBox richTextBox10;
+        private RichTextBox richTextBox11;
+        private RichTextBox richTextBox12;
+        private RichTextBox richTextBox13;
+        private RichTextBox richTextBox14;
+        private RichTextBox richTextBox15;
+        private RichTextBox richTextBox16;
+        private RichTextBox richTextBox17;
+        private Label label1;
+        private Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,25 @@
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox19_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+}
